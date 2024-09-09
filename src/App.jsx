6f70c8aad8bb90687399da76a050ae805cee169b
shivsharcode <shivshar16@outlook.com>
2024-09-10 01:36:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import DigitalClock from "./colorPicker/DigitalClock/DigitalClock.jsx"
+import ColorPicker from "./colorPicker/ColorPicker.jsx"
+
+
+function App(){
+
+  return (
+    <>
+      <DigitalClock></DigitalClock>
+      {/* <ColorPicker></ColorPicker> */}
+    </>
+  )
+}
+
+export default App
